12df1abb912b9775cbdf7109d3fc4ae4e5d58a96
Gizbreht <129413209+Gizbreht@users.noreply.github.com>
2023-04-05 17:19:44 +0300

    Add files via upload

---
+++ лаб1.py
@@ -0,0 +1,8 @@
+number1 = int(input("введите первое число: ")) 
+number2 = int(input("введите второе число: ")) 
+if number1 > number2 * 3: 
+ print("a » b") 
+elif number1 > number2: 
+ print("a > b") 
+elif number1 < number2: 
+ print("a < b")
