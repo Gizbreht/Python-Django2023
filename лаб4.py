eafd3967c160aee381bd10e2256a37727b68e45d
Gizbreht <129413209+Gizbreht@users.noreply.github.com>
2023-04-14 13:39:31 +0300

    Add files via upload

---
+++ лаб4.py
@@ -0,0 +1,73 @@
+chudle
+/
+Python-Django
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+Python-Django/n4.py
+@chudle
+chudle Мини-калькулятор
+1 contributor
+57 lines (56 sloc) 1.43 KB
+import random
+import math
+
+print("+ - * / ^ | R ! C q\n")
+while True:
+op = input()
+match op:
+case "+":
+x = float(input("X - "))
+y = float(input("Y - "))
+print(x + y)
+case "-":
+x = float(input("X - "))
+y = float(input("Y - "))
+print(x - y)
+case "*":
+x = float(input("X - "))
+y = float(input("Y - "))
+print(x * y)
+case "/":
+x = float(input("X - "))
+y = float(input("Y - "))
+if y != 0:
+print(x / y)
+else:
+print("?")
+case "^":
+x = float(input("X - "))
+y = float(input("Y - "))
+print(x ** y)
+case "|":
+x = float(input("X - "))
+print(abs(x))
+case "R":
+a = float(input("A - "))
+b = float(input("B - "))
+if a <= b:
+print(random.randint(a, b))
+else:
+print("?")
+case "!":
+x = int(input("X - "))
+if x >= 0:
+print(math.factorial(x))
+else:
+print("?")
+case "C":
+x = float(input("X - "))
+if abs(x) <= 1:
+print(math.acos(x))
+else:
+print("?")
+case "q":
+break
+case default:
+print("?")
+print("")
